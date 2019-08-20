e547339e96af9f311e4ce0cd4a15b01113dd5ede
condy <condy0919@gmail.com>
2019-08-20 22:42:34 +0800

    backport some concepts in old style

---
+++ bipolar/traits.hpp
@@ -0,0 +1,143 @@
+#ifndef BIPOLAR_TRAITS_HPP_
+#define BIPOLAR_TRAITS_HPP_
+
+/// \file traits.hpp
+/// \see https://en.cppreference.com/w/cpp/header/concepts
+/// Backports some concepts in old style for compatibility
+
+#include <type_traits>
+
+namespace bipolar {
+namespace detail {
+template <typename, typename, template <typename, typename> typename,
+          typename = void>
+struct has_relation : std::false_type {};
+
+template <typename T, typename U, template <typename, typename> typename Op>
+struct has_relation<T, U, Op, std::enable_if_t<std::is_same_v<Op<T, U>, bool>>>
+    : std::true_type {};
+
+template <typename T, typename U, template <typename, typename> typename Op>
+inline constexpr bool has_relation_v = has_relation<T, U, Op>::value;
+
+template <typename T, typename U>
+using equal_to_t = decltype(std::declval<T>() == std::declval<U>());
+
+template <typename T, typename U>
+using not_equal_t = decltype(std::declval<T>() != std::declval<U>());
+
+template <typename T, typename U>
+using less_than_t = decltype(std::declval<T>() < std::declval<U>());
+
+template <typename T, typename U>
+using less_than_or_equal_to_t = decltype(std::declval<T>() <= std::declval<U>());
+
+template <typename T, typename U>
+using greater_than_t = decltype(std::declval<T>() > std::declval<U>());
+
+template <typename T, typename U>
+using greater_than_or_equal_to_t = decltype(std::declval<T>() >= std::declval<U>());
+} // namespace detail
+
+/// @{
+/// \brief This trait tests equal to and is used by both operator== and
+/// operator!=
+///
+/// \tparam T
+/// \tparam U
+template <typename T, typename U = T>
+struct is_equality_comparable
+    : std::bool_constant<detail::has_relation_v<T, U, detail::equal_to_t> &&
+                         detail::has_relation_v<T, U, detail::not_equal_t> &&
+                         detail::has_relation_v<U, T, detail::equal_to_t> &&
+                         detail::has_relation_v<U, T, detail::not_equal_t>> {};
+
+template <typename T, typename U = T>
+inline constexpr bool is_equality_comparable_v = is_equality_comparable<T, U>::value;
+/// @}
+
+
+/// @{
+/// \brief This trait tests less than and is used by the operator<=
+///
+/// \tparam T
+/// \tparam U
+template <typename T, typename U = T>
+struct is_less_than_comparable
+    : detail::has_relation<T, U, detail::less_than_t> {};
+
+template <typename T, typename U = T>
+inline constexpr bool is_less_than_comparable_v =
+    is_less_than_comparable<T, U>::value;
+/// @}
+
+/// @{
+/// \brief This trait tests less than or equal to and is used by the operator <=
+///
+/// \tparam T
+/// \tparam U
+template <typename T, typename U = T>
+struct is_less_than_or_equal_to_comparable
+    : detail::has_relation<T, U, detail::less_than_or_equal_to_t> {};
+
+template <typename T, typename U = T>
+inline constexpr bool is_less_than_or_equal_to_comparable_v =
+    is_less_than_or_equal_to_comparable<T, U>::value;
+/// @}
+
+/// @{
+/// \brief This trait tests greater than and is used by the operator>
+///
+/// \tparam T
+/// \tparam U
+template <typename T, typename U = T>
+struct is_greater_than_comparable
+    : detail::has_relation<T, U, detail::greater_than_t> {};
+
+template <typename T, typename U = T>
+inline constexpr bool is_greater_than_comparable_v =
+    is_greater_than_comparable<T, U>::value;
+/// @}
+
+
+/// @{
+/// \brief This trait tests greater than or equal to and is used by the
+/// operator>=
+///
+/// \tparam T
+/// \tparam U
+template <typename T, typename U = T>
+struct is_greater_than_or_equal_to_comparable
+    : detail::has_relation<T, U, detail::greater_than_or_equal_to_t> {};
+
+template <typename T, typename U = T>
+inline constexpr bool is_greater_than_or_equal_to_comparable_v =
+    is_greater_than_or_equal_to_comparable<T, U>::value;
+/// @}
+
+
+/// @{
+/// \brief This trait tests \c T is strict totally ordered with \c U
+///
+/// \tparam T
+/// \tparam U
+template <typename T, typename U = T>
+struct is_strict_totally_ordered
+    : std::bool_constant<is_equality_comparable_v<T, U> &&
+                         is_less_than_comparable_v<T, U> &&
+                         is_greater_than_comparable_v<T, U> &&
+                         is_less_than_or_equal_to_comparable_v<T, U> &&
+                         is_greater_than_or_equal_to_comparable_v<T, U> &&
+                         is_less_than_comparable_v<U, T> &&
+                         is_greater_than_comparable_v<U, T> &&
+                         is_less_than_or_equal_to_comparable_v<U, T> &&
+                         is_greater_than_or_equal_to_comparable_v<U, T>> {};
+
+template <typename T, typename U = T>
+inline constexpr bool is_strict_totally_ordered_v =
+    is_strict_totally_ordered<T, U>::value;
+/// @}
+
+} // namespace bipolar
+
+#endif
---
+++ tests/traits_test.cpp
@@ -0,0 +1,46 @@
+#include "bipolar/traits.hpp"
+#include <gtest/gtest.h>
+
+using namespace bipolar;
+
+struct NoEquality {};
+static_assert(!is_equality_comparable_v<NoEquality>);
+
+struct IncompleteEquality {};
+bool operator==(const IncompleteEquality&, const IncompleteEquality&);
+static_assert(!is_equality_comparable_v<IncompleteEquality>);
+
+struct IncompleteEquality2 {};
+int operator==(const IncompleteEquality2&, const IncompleteEquality2&);
+bool operator!=(const IncompleteEquality2&, const IncompleteEquality2&);
+static_assert(!is_equality_comparable_v<IncompleteEquality2>);
+
+struct Equality {};
+bool operator==(const Equality&, const Equality&);
+bool operator!=(const Equality&, const Equality&);
+static_assert(is_equality_comparable_v<Equality>);
+
+struct LessThan {};
+bool operator<(const LessThan&, const LessThan&);
+static_assert(is_less_than_comparable_v<LessThan>);
+
+struct LessThanOrEqualTo {};
+bool operator<=(const LessThanOrEqualTo&, const LessThanOrEqualTo&);
+static_assert(is_less_than_or_equal_to_comparable_v<LessThanOrEqualTo>);
+
+struct GreaterThan {};
+bool operator>(const GreaterThan&, const GreaterThan&);
+static_assert(is_greater_than_comparable_v<GreaterThan>);
+
+struct GreaterThanOrEqualTo {};
+bool operator>=(const GreaterThanOrEqualTo&, const GreaterThanOrEqualTo&);
+static_assert(is_greater_than_or_equal_to_comparable_v<GreaterThanOrEqualTo>);
+
+struct TotallyOrdered {};
+bool operator==(const TotallyOrdered&, const TotallyOrdered&);
+bool operator!=(const TotallyOrdered&, const TotallyOrdered&);
+bool operator<(const TotallyOrdered&, const TotallyOrdered&);
+bool operator<=(const TotallyOrdered&, const TotallyOrdered&);
+bool operator>(const TotallyOrdered&, const TotallyOrdered&);
+bool operator>=(const TotallyOrdered&, const TotallyOrdered&);
+static_assert(is_strict_totally_ordered_v<TotallyOrdered>);
