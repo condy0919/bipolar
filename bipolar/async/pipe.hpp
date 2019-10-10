e63c9a89079aa8e713542fc1bb415c621518f36f
condy <condy0919@gmail.com>
2019-10-11 00:05:50 +0800

    [async] make async pipable

--- bipolar/async/pipe.hpp
+++ bipolar/async/pipe.hpp
@@ -1,18 +1,33 @@
-/// \file pipe.hpp
-/// \brief operator| for pipable types
-/// \see Pipable
-/// \see pipe
+//! Pipe
+//!
+//! # Brief
+//!
+//! Pipe introduces composability which allows Senders, Receivers,
+//! and Executors to compose.
+//!
+//! Composability involves creating functions in the pattern
+//! `Executor(Executor)` and `Continuation(Continuation)` that adapt one
+//! implementation into another while composingsome additional functionality.
+//!
+//! Composability involves creating functions that compose sets of adaptors in
+//! the pattern of `Executor operator|(Executor, Executor(Executor))` and
+//! `Executor pipe(Executor, Executor(Executor)...)`.
+//!
+//! # Examples
+//!
+//! see `Pipable` and `pipe` for examples
 
 #ifndef BIPOLAR_ASYNC_PIPE_HPP_
 #define BIPOLAR_ASYNC_PIPE_HPP_
 
-#include <type_traits>
 #include <utility>
+#include <type_traits>
 
 namespace bipolar {
-/// \struct Pipable
-/// \brief Makes `operator|` visiable to Pipable types by ADL
-/// \see https://en.cppreference.com/w/cpp/language/adl
+/// The `Pipable` base class
+///
+/// It makes `operator|` visiable to Pipable types by ADL.
+/// see https://en.cppreference.com/w/cpp/language/adl for details.
 ///
 /// # Examples
 ///
@@ -25,7 +40,8 @@ namespace bipolar {
 /// ```
 struct Pipable {};
 
-/// \brief operator| overload for Pipable types
+/// `operator|` for pipable types
+///
 /// T can be the following types:
 /// - Sender
 /// - Receiver
@@ -36,19 +52,17 @@ constexpr auto operator|(T&& t, Op&& op) {
     return op(std::forward<T>(t));
 }
 
-/// \struct PipeFn
-/// \brief pipe the functions explicitly
-/// \see pipe
+/// The `pipe` functor
+///
+/// Same with `t | op0 | op1 | op2 | ... | op`
 struct PipeFn {
-	template <typename T, typename... Fns>
-	constexpr auto operator()(T&& t, Fns&&... fns) const {
-		return (std::forward<T>(t) | ... | std::forward<Fns>(fns));
-	}
+    template <typename T, typename... Fns>
+    constexpr auto operator()(T&& t, Fns&&... fns) const {
+        return (std::forward<T>(t) | ... | std::forward<Fns>(fns));
+    }
 };
 
-/// \var pipe
-/// \brief
-inline constexpr auto pipe = PipeFn{};
+inline constexpr PipeFn pipe{};
 
 } // namespace bipolar
 
