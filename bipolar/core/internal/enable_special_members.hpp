07bd2866325dff671579e7d85bcc11f661b6c0e2
condy <condy0919@gmail.com>
2019-11-14 21:41:09 +0800

    [core] split copy/move into ctor and assignment

--- bipolar/core/internal/enable_special_members.hpp
+++ bipolar/core/internal/enable_special_members.hpp
@@ -10,110 +10,47 @@ namespace internal {
 
 /// EnableDefaultConstructor
 ///
-/// A mixin helper to conditionally enable/disable the default constructor
-template <bool Enable, typename Tag = void>
+/// A mixin helper to conditionally enable the default constructor
+template <bool Enable, bool Noexcept = true, typename Tag = void>
 struct EnableDefaultConstructor;
-
-/// DisableDefaultConstructor
-///
-/// A mixin helper to disable the default constructor
-template <typename Tag = void>
-using DisableDefaultConstructor = EnableDefaultConstructor<false, Tag>;
 
 /// EnableDestructor
 ///
-/// A mixin helper to conditionally enable/disable the destructor
-template <bool Enable, typename Tag = void>
+/// A mixin helper to conditionally enable the destructor
+template <bool Enable, bool Noexcept = true, typename Tag = void>
 struct EnableDestructor;
 
-/// DisableDestructor
+/// EnableCopyConstructor
 ///
-/// A mixin helper to disable the destructor
-template <typename Tag = void>
-using DisableDestructor = EnableDestructor<false, Tag>;
+/// A mixin helper to conditionally enable copy constructor
+template <bool Enable, bool Noexcept = true, typename Tag = void>
+struct EnableCopyConstructor;
 
-/// EnableCopy
+/// EnableCopyAssignment
 ///
-/// A mixin helper to conditionally enable/disable
-/// - copy constructor
-/// - copy assignment
-template <bool Copy, bool CopyAssignment, typename Tag = void>
-struct EnableCopy;
+/// A mixin helper to conditionally enable copy assignment
+template <bool Enable, bool Noexcept = true, typename Tag = void>
+struct EnableCopyAssignment;
 
-/// DisableCopy
+/// EnableMoveConstructor
 ///
-/// A mixin helper to disable
-/// - copy constructor
-/// - copy assignment
-template <typename Tag = void>
-using DisableCopy = EnableCopy<false, false, Tag>;
+/// A mixin helper to conditionally enable move constructor
+template <bool Enable, bool Noexcept = true, typename Tag = void>
+struct EnableMoveConstructor;
 
-/// EnableMove
-///
-/// A mixin helper to conditionally enable/disable
-/// - move constructor
-/// - move assignment
-template <bool Move, bool MoveAssignment, typename Tag = void>
-struct EnableMove;
-
-/// DisableMove
-///
-/// A mixin helper to disbale
-/// - move constructor
-/// - move assignment
-template <typename Tag = void>
-using DisableMove = EnableMove<false, false, Tag>;
-
-/// EnableCopyMove
-///
-/// A mixin helper to conditionally enable/disable
-/// - copy constructor
-/// - move constructor
-/// - copy assignment
-/// - move assignment
-///
-/// The `Tag` template parameter is to make mixin base unique and thus avoid
-/// ambiguities
-template <bool Copy, bool CopyAssignment, bool Move, bool MoveAssignment,
-          typename Tag = void>
-struct EnableCopyMove : private EnableCopy<Copy, CopyAssignment, Tag>,
-                        private EnableMove<Move, MoveAssignment, Tag> {};
-
-/// A mixin helper to disbale
-/// - copy constructor
-/// - move constructor
-/// - copy assignment
-/// - move assignment
-template <typename Tag = void>
-using DisableCopyMove = EnableCopyMove<false, false, false, false, Tag>;
-
-/// EnableSpecialMembers
-///
-/// A mixin helper to conditionally enable/disable the special members
-///
-/// The `Tag` template parameter it to make mixin base unique and thus avoid
-/// ambiguities
-template <bool Default, bool Destructor, bool Copy, bool CopyAssignment,
-          bool Move, bool MoveAssignment, typename Tag = void>
-struct EnableSpecialMembers : private EnableDefaultConstructor<Default, Tag>,
-                              private EnableDestructor<Destructor, Tag>,
-                              private EnableCopy<Copy, CopyAssignment, Tag>,
-                              private EnableMove<Move, MoveAssignment, Tag> {};
-
-/// DisableSpecialMembers
-///
-/// A mixin helper to disbale all the special members
-template <typename Tag = void>
-using DisableSpecialMembers =
-    EnableSpecialMembers<false, false, false, false, false, false, Tag>;
+/// EnableMoveAssignment
+/// 
+/// A mixin helper to conditionally enable move assignment
+template <bool Enable, bool Noexcept = true, typename Tag = void>
+struct EnableMoveAssignment;
 
 // clang-format off
 
 // Boilerplate follows
 
 // EnableDefaultConstructor specialization
-template <typename Tag>
-struct EnableDefaultConstructor<false, Tag> {
+template <bool Noexcept, typename Tag>
+struct EnableDefaultConstructor<false, Noexcept, Tag> {
     constexpr EnableDefaultConstructor() noexcept                                           = delete;
     constexpr EnableDefaultConstructor(const EnableDefaultConstructor&) noexcept            = default;
     constexpr EnableDefaultConstructor(EnableDefaultConstructor&&) noexcept                 = default;
@@ -121,9 +58,9 @@ struct EnableDefaultConstructor<false, Tag> {
     constexpr EnableDefaultConstructor& operator=(EnableDefaultConstructor&&) noexcept      = default;
 };
 
-template <typename Tag>
-struct EnableDefaultConstructor<true, Tag> {
-    constexpr EnableDefaultConstructor() noexcept                                           = default;
+template <bool Noexcept, typename Tag>
+struct EnableDefaultConstructor<true, Noexcept, Tag> {
+    constexpr EnableDefaultConstructor() noexcept(Noexcept)                                 = default;
     constexpr EnableDefaultConstructor(const EnableDefaultConstructor&) noexcept            = default;
     constexpr EnableDefaultConstructor(EnableDefaultConstructor&&) noexcept                 = default;
     constexpr EnableDefaultConstructor& operator=(const EnableDefaultConstructor&) noexcept = default;
@@ -131,88 +68,91 @@ struct EnableDefaultConstructor<true, Tag> {
 };
 
 // EnableDestructor specialization
-template <typename Tag>
-struct EnableDestructor<false, Tag> {
+template <bool Noexcept, typename Tag>
+struct EnableDestructor<false, Noexcept, Tag> {
     ~EnableDestructor() noexcept = delete;
 };
 
-template <typename Tag>
-struct EnableDestructor<true, Tag> {
-    ~EnableDestructor() noexcept = default;
+template <bool Noexcept, typename Tag>
+struct EnableDestructor<true, Noexcept, Tag> {
+    ~EnableDestructor() noexcept(Noexcept) = default;
 };
 
-// EnableCopy specialization
-template <typename Tag>
-struct EnableCopy<false, false, Tag> {
-    constexpr EnableCopy() noexcept                             = default;
-    constexpr EnableCopy(const EnableCopy&) noexcept            = delete;
-    constexpr EnableCopy(EnableCopy&&) noexcept                 = default;
-    constexpr EnableCopy& operator=(const EnableCopy&) noexcept = delete;
-    constexpr EnableCopy& operator=(EnableCopy&&) noexcept      = default;
+// EnableCopyConstructor specialization
+template <bool Noexcept, typename Tag>
+struct EnableCopyConstructor<false, Noexcept, Tag> {
+    constexpr EnableCopyConstructor() noexcept                                        = default;
+    constexpr EnableCopyConstructor(const EnableCopyConstructor&) noexcept            = delete;
+    constexpr EnableCopyConstructor(EnableCopyConstructor&&) noexcept                 = default;
+    constexpr EnableCopyConstructor& operator=(const EnableCopyConstructor&) noexcept = default;
+    constexpr EnableCopyConstructor& operator=(EnableCopyConstructor&&) noexcept      = default;
 };
 
-template <typename Tag>
-struct EnableCopy<true, false, Tag> {
-    constexpr EnableCopy() noexcept                             = default;
-    constexpr EnableCopy(const EnableCopy&) noexcept            = default;
-    constexpr EnableCopy(EnableCopy&&) noexcept                 = default;
-    constexpr EnableCopy& operator=(const EnableCopy&) noexcept = delete;
-    constexpr EnableCopy& operator=(EnableCopy&&) noexcept      = default;
+template <bool Noexcept, typename Tag>
+struct EnableCopyConstructor<true, Noexcept, Tag> {
+    constexpr EnableCopyConstructor() noexcept                                        = default;
+    constexpr EnableCopyConstructor(const EnableCopyConstructor&) noexcept(Noexcept)  = default;
+    constexpr EnableCopyConstructor(EnableCopyConstructor&&) noexcept                 = default;
+    constexpr EnableCopyConstructor& operator=(const EnableCopyConstructor&) noexcept = default;
+    constexpr EnableCopyConstructor& operator=(EnableCopyConstructor&&) noexcept      = default;
 };
 
-template <typename Tag>
-struct EnableCopy<false, true, Tag> {
-    constexpr EnableCopy() noexcept                             = default;
-    constexpr EnableCopy(const EnableCopy&) noexcept            = delete;
-    constexpr EnableCopy(EnableCopy&&) noexcept                 = default;
-    constexpr EnableCopy& operator=(const EnableCopy&) noexcept = default;
-    constexpr EnableCopy& operator=(EnableCopy&&) noexcept      = default;
+// EnableCopyAssignment specialization
+template <bool Noexcept, typename Tag>
+struct EnableCopyAssignment<false, Noexcept, Tag> {
+    constexpr EnableCopyAssignment() noexcept                                       = default;
+    constexpr EnableCopyAssignment(const EnableCopyAssignment&) noexcept            = default;
+    constexpr EnableCopyAssignment(EnableCopyAssignment&&) noexcept                 = default;
+    constexpr EnableCopyAssignment& operator=(const EnableCopyAssignment&) noexcept = delete;
+    constexpr EnableCopyAssignment& operator=(EnableCopyAssignment&&) noexcept      = default;
 };
 
-template <typename Tag>
-struct EnableCopy<true, true, Tag> {
-    constexpr EnableCopy() noexcept                             = default;
-    constexpr EnableCopy(const EnableCopy&) noexcept            = default;
-    constexpr EnableCopy(EnableCopy&&) noexcept                 = default;
-    constexpr EnableCopy& operator=(const EnableCopy&) noexcept = default;
-    constexpr EnableCopy& operator=(EnableCopy&&) noexcept      = default;
+template <bool Noexcept, typename Tag>
+struct EnableCopyAssignment<true, Noexcept, Tag> {
+    constexpr EnableCopyAssignment() noexcept                                                 = default;
+    constexpr EnableCopyAssignment(const EnableCopyAssignment&) noexcept                      = default;
+    constexpr EnableCopyAssignment(EnableCopyAssignment&&) noexcept                           = default;
+    constexpr EnableCopyAssignment& operator=(const EnableCopyAssignment&) noexcept(Noexcept) = default;
+    constexpr EnableCopyAssignment& operator=(EnableCopyAssignment&&) noexcept                = default;
 };
 
-// EnableMove specialization
-template <typename Tag>
-struct EnableMove<false, false, Tag> {
-    constexpr EnableMove() noexcept                             = default;
-    constexpr EnableMove(const EnableMove&) noexcept            = default;
-    constexpr EnableMove(EnableMove&&) noexcept                 = delete;
-    constexpr EnableMove& operator=(const EnableMove&) noexcept = default;
-    constexpr EnableMove& operator=(EnableMove&&) noexcept      = delete;
+// EnableMoveConstructor specialization
+template <bool Noexcept, typename Tag>
+struct EnableMoveConstructor<false, Noexcept, Tag> {
+    constexpr EnableMoveConstructor() noexcept                                        = default;
+    constexpr EnableMoveConstructor(const EnableMoveConstructor&) noexcept            = default;
+    constexpr EnableMoveConstructor(EnableMoveConstructor&&) noexcept                 = delete;
+    constexpr EnableMoveConstructor& operator=(const EnableMoveConstructor&) noexcept = default;
+    constexpr EnableMoveConstructor& operator=(EnableMoveConstructor&&) noexcept      = default;
 };
 
-template <typename Tag>
-struct EnableMove<true, false, Tag> {
-    constexpr EnableMove() noexcept                             = default;
-    constexpr EnableMove(const EnableMove&) noexcept            = default;
-    constexpr EnableMove(EnableMove&&) noexcept                 = default;
-    constexpr EnableMove& operator=(const EnableMove&) noexcept = default;
-    constexpr EnableMove& operator=(EnableMove&&) noexcept      = delete;
+
+template <bool Noexcept, typename Tag>
+struct EnableMoveConstructor<true, Noexcept, Tag> {
+    constexpr EnableMoveConstructor() noexcept                                        = default;
+    constexpr EnableMoveConstructor(const EnableMoveConstructor&) noexcept            = default;
+    constexpr EnableMoveConstructor(EnableMoveConstructor&&) noexcept(Noexcept)       = default;
+    constexpr EnableMoveConstructor& operator=(const EnableMoveConstructor&) noexcept = default;
+    constexpr EnableMoveConstructor& operator=(EnableMoveConstructor&&) noexcept      = default;
 };
 
-template <typename Tag>
-struct EnableMove<false, true, Tag> {
-    constexpr EnableMove() noexcept                             = default;
-    constexpr EnableMove(const EnableMove&) noexcept            = default;
-    constexpr EnableMove(EnableMove&&) noexcept                 = delete;
-    constexpr EnableMove& operator=(const EnableMove&) noexcept = default;
-    constexpr EnableMove& operator=(EnableMove&&) noexcept      = default;
+// EnableMoveAssignment specialization
+template <bool Noexcept, typename Tag>
+struct EnableMoveAssignment<false, Noexcept, Tag> {
+    constexpr EnableMoveAssignment() noexcept                                       = default;
+    constexpr EnableMoveAssignment(const EnableMoveAssignment&) noexcept            = default;
+    constexpr EnableMoveAssignment(EnableMoveAssignment&&) noexcept                 = default;
+    constexpr EnableMoveAssignment& operator=(const EnableMoveAssignment&) noexcept = default;
+    constexpr EnableMoveAssignment& operator=(EnableMoveAssignment&&) noexcept      = delete;
 };
 
-template <typename Tag>
-struct EnableMove<true, true, Tag> {
-    constexpr EnableMove() noexcept                             = default;
-    constexpr EnableMove(const EnableMove&) noexcept            = default;
-    constexpr EnableMove(EnableMove&&) noexcept                 = default;
-    constexpr EnableMove& operator=(const EnableMove&) noexcept = default;
-    constexpr EnableMove& operator=(EnableMove&&) noexcept      = default;
+template <bool Noexcept, typename Tag>
+struct EnableMoveAssignment<true, Noexcept, Tag> {
+    constexpr EnableMoveAssignment() noexcept                                            = default;
+    constexpr EnableMoveAssignment(const EnableMoveAssignment&) noexcept                 = default;
+    constexpr EnableMoveAssignment(EnableMoveAssignment&&) noexcept                      = default;
+    constexpr EnableMoveAssignment& operator=(const EnableMoveAssignment&) noexcept      = default;
+    constexpr EnableMoveAssignment& operator=(EnableMoveAssignment&&) noexcept(Noexcept) = default;
 };
 // clang-format on
 
