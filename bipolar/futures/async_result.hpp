1533beeeb354917d323c44a67854c701185d4ded
condy <condy0919@gmail.com>
2019-11-18 22:16:45 +0800

    [futures] AsyncResult: re-write it in core's Result & test it

--- bipolar/futures/async_result.hpp
+++ bipolar/futures/async_result.hpp
@@ -1,16 +1,16 @@
 //! AsyncResult
 //!
-//! `AsyncResult` is similar to `Result` except for allowing an empty state.
+//! `AsyncResult` is a wrapper to `Result` except for allowing an empty state.
 //!
 //! See documentation of `Result` for more information.
 
 #ifndef BIPOLAR_FUTURES_ASYNC_RESULT_HPP_
 #define BIPOLAR_FUTURES_ASYNC_RESULT_HPP_
 
-#include <utility>
-#include <variant>
 #include <type_traits>
+#include <utility>
 
+#include "bipolar/core/result.hpp"
 #include "bipolar/core/traits.hpp"
 
 namespace bipolar {
@@ -18,13 +18,27 @@ namespace bipolar {
 template <typename, typename>
 class AsyncResult;
 
-struct AsyncPending;
+/// Represents the intermediate state of a `AsyncResult` that has not yet
+/// completed.
+struct AsyncPending {};
 
-template <typename>
-struct AsyncOk;
+/// Represents the result of a successful task.
+template <typename T>
+struct AsyncOk {
+    constexpr /*implicit*/ AsyncOk(T&& val) : value(std::move(val)) {}
+    constexpr /*implicit*/ AsyncOk(const T& val) : value(val) {}
 
-template <typename>
-struct AsyncError;
+    T value;
+};
+
+/// Represents the result of a failed task.
+template <typename E>
+struct AsyncError {
+    constexpr /*implicit*/ AsyncError(E&& err) : error(std::move(err)) {}
+    constexpr /*implicit*/ AsyncError(const E& err) : error(err) {}
+
+    E error;
+};
 
 namespace detail {
 // AsyncResult helper traits
@@ -56,143 +70,108 @@ template <typename T>
 inline constexpr bool is_async_error_v = is_async_error<T>::value;
 } // namespace detail
 
-/// Represents the intermediate state of a `AsyncResult` that has not yet
-/// completed.
-struct AsyncPending {};
-
-/// Represents the result of a successful task.
-template <typename T>
-struct AsyncOk {
-    constexpr /*implicit*/ AsyncOk(T&& val) : value(std::move(val)) {}
-
-    constexpr /*implicit*/ AsyncOk(const T& val) : value(val) {}
-
-    T value;
-};
-
-/// Represents the result of a failed task.
-template <typename E>
-struct AsyncError {
-    constexpr /*implicit*/ AsyncError(E&& err) : error(std::move(err)) {}
-
-    constexpr /*implicit*/ AsyncError(const E& err) : error(err) {}
-
-    E error;
-};
-
+/// AsyncResult
+///
+/// `AsyncResult` represents the result of a task which may have succeeded,
+/// failed or still be in progress.
+///
+/// Use `AsyncPending{}`, `AsyncOk{}`, or `AsyncError{}` to initialize the
+/// result.
+///
+/// # Examples
+///
+/// ```
+/// AsyncResult<int, std::string> good = AsyncOk(13);
+///
+/// do_with_result(std::move(good));
+/// assert(good.is_pending());
+/// ```
 template <typename T, typename E>
-class AsyncResult {
+class AsyncResult final {
 public:
-    enum class State {
-        PENDING,
-        OK,
-        ERROR,
-    };
-
     using value_type = T;
     using error_type = E;
 
     /// Creates a pending result
-    constexpr AsyncResult() = default;
-    constexpr /*implicit*/ AsyncResult(AsyncPending) {}
+    constexpr AsyncResult() noexcept : result_(make_empty_result<T, E>()) {}
+    constexpr /*implicit*/ AsyncResult(AsyncPending) noexcept : AsyncResult() {}
 
-    /// @{
     /// Creates an ok result
-    template <typename U,
-              std::enable_if_t<std::is_constructible_v<T, U>, int> = 0>
-    constexpr /*implicit*/ AsyncResult(AsyncOk<U>&& ok)
-        : state_(std::in_place_index<1>, std::move(ok.value)) {}
+    constexpr /*implicit*/ AsyncResult(AsyncOk<T>&& ok) noexcept(
+        std::is_nothrow_move_constructible_v<T>)
+        : result_(Ok(std::move(ok.value))) {}
 
-    template <typename U,
-              std::enable_if_t<std::is_constructible_v<T, U>, int> = 0>
-    constexpr /*implicit*/ AsyncResult(const AsyncOk<U>& ok)
-        : state_(std::in_place_index<1>, ok.value) {}
-    /// @}
+    constexpr /*implicit*/ AsyncResult(const AsyncOk<T>& ok) noexcept(
+        std::is_nothrow_copy_constructible_v<T>)
+        : result_(Ok(ok.value)) {}
 
-    /// @{
     /// Creates an error result
-    template <typename U,
-              std::enable_if_t<std::is_constructible_v<E, U>, int> = 0>
-    constexpr /*implicit*/ AsyncResult(AsyncError<U>&& err)
-        : state_(std::in_place_index<2>, std::move(err.value)) {}
+    constexpr /*implicit*/ AsyncResult(AsyncError<E>&& err) noexcept(
+        std::is_nothrow_move_constructible_v<E>)
+        : result_(Err(std::move(err.error))) {}
 
-    template <typename U,
-              std::enable_if_t<std::is_constructible_v<E, U>, int> = 0>
-    constexpr /*implicit*/ AsyncResult(const AsyncError<U>& err)
-        : state_(std::in_place_index<2>, err.value) {}
-    /// @}
+    constexpr /*implicit*/ AsyncResult(const AsyncError<E>& err) noexcept(
+        std::is_nothrow_copy_constructible_v<E>)
+        : result_(Err(err.error)) {}
 
     /// Moves from another result, leaving the other one in a pending state
-    constexpr AsyncResult(AsyncResult&& rhs) : state_(std::move(rhs.state_)) {
+    constexpr AsyncResult(AsyncResult&& rhs) : result_(std::move(rhs.result_)) {
         rhs.reset();
     }
 
-    /// Copies another result (if copyable)
+    /// Copies/Assigns another result (if copyable)
     constexpr AsyncResult(const AsyncResult&) = default;
+    constexpr AsyncResult& operator=(const AsyncResult&) = default;
 
     ~AsyncResult() = default;
 
-    /// Assigns from another result (if copyable)
-    constexpr AsyncResult& operator=(const AsyncResult&) = default;
-
     /// Moves from another result, leaving the other one in a pending state
     constexpr AsyncResult& operator=(AsyncResult&& rhs) {
-        state_ = std::move(rhs.state_);
+        result_ = std::move(rhs.result_);
         rhs.reset();
         return *this;
     }
 
-    /// Returns the state of the task's result: pending, ok or error
-    constexpr State state() const {
-        return static_cast<State>(state_.index());
-    }
-
-    /// Returns true if the result is not pending
-    constexpr explicit operator bool() const {
-        return !is_pending();
-    }
-
     /// Returns true if the result is still in progress
     constexpr bool is_pending() const {
-        return state() == State::PENDING;
+        return has_nothing(result_);
     }
 
     /// Returns true if the task succeeded
     constexpr bool is_ok() const {
-        return state() == State::OK;
+        return result_.has_value();
     }
 
     /// Returns true if the task failed
     constexpr bool is_error() const {
-        return state() == State::ERROR;
+        return result_.has_error();
     }
 
-    /// @{
     /// Gets the result's value
-    /// Asserts that the result's state is `State::OK`
+    ///
+    /// Throws `BadResultAccess` when `is_ok()` is false.
     constexpr T& value() {
-        return std::get<1>(state_);
+        return result_.value();
     }
 
     constexpr const T& value() const {
-        return std::get<1>(state_);
+        return result_.value();
     }
-    /// @}
 
-    /// @{
     /// Gets the result's error
-    /// Asserts that the result's state is `State::ERROR`
+    ///
+    /// Throws `BadResultAccess` when `is_error()` is false.
     constexpr E& error() {
-        return std::get<2>(state_);
+        return result_.error();
     }
 
     constexpr const E& error() const {
-        return std::get<2>(state_);
+        return result_.error();
     }
-    /// @}
 
     /// Takes the result's value, leaving it in a pending state
-    /// Asserts that the result's state is `State::OK`
+    ///
+    /// Throws `BadResultAccess` when `is_ok()` is false.
     constexpr T take_value() {
         auto ret = std::move(value());
         reset();
@@ -200,7 +179,8 @@ public:
     }
 
     /// Takes the result's error, leaving it in a pending state
-    /// Asserts that the result's state is `State::ERROR`
+    ///
+    /// Throws `BadResultAccess` when `is_error()` is false.
     constexpr E take_error() {
         auto ret = std::move(error());
         reset();
@@ -209,17 +189,26 @@ public:
 
     /// Resets `AsyncResult`
     constexpr void reset() {
-        state_.template emplace<0>();
+        result_.assign(Empty{});
     }
 
     /// Swaps `AsyncResult`
-    constexpr void swap(AsyncResult& rhs) {
-        state_.swap(rhs);
+    constexpr void swap(AsyncResult& rhs) noexcept(
+        detail::all_of_v<std::is_nothrow_swappable, T, E>) {
+        result_.swap(rhs.result_);
     }
 
 private:
-    std::variant<std::monostate, T, E> state_;
+    Result<T, E> result_;
 };
+
+/// Swaps
+template <typename T, typename E>
+inline constexpr void
+swap(AsyncResult<T, E>& lhs, AsyncResult<T, E>& rhs) noexcept(
+    detail::all_of_v<std::is_nothrow_swappable, T, E>) {
+    lhs.swap(rhs);
+}
 
 } // namespace bipolar
 
