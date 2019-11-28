65b5a9e489d1d0ab53c9741cbce1756f30f4448d
condy <condy0919@gmail.com>
2019-11-28 08:45:40 +0800

    [executors] InlineExecutor

---
+++ bipolar/executors/inline_executor.hpp
@@ -0,0 +1,75 @@
+//! InlineExecutor
+//!
+//! See `InlineExecutor` for details
+//!
+
+#ifndef BIPOLAR_EXECUTORS_INLINE_EXECUTOR_HPP_
+#define BIPOLAR_EXECUTORS_INLINE_EXECUTOR_HPP_
+
+#include "bipolar/futures/executor.hpp"
+
+#include <cassert>
+#include <stdexcept>
+
+#include <boost/noncopyable.hpp>
+
+namespace bipolar {
+/// InlineExecutor
+///
+/// An executor which always creates execution "inline".
+/// Therefore, the execution it creates always blocks the execution of its
+/// client.
+///
+/// # Examples
+///
+/// ```
+/// InlineExecutor inline_executor;
+///
+/// auto p = make_ok_promise<std::string, int>("inline")
+///     .and_then([](const std::string& s) {
+///         return AsyncOk(s.size());
+///     })
+///     .then([](const AsyncResult<std::size_t, int>& result) {
+///         assert(result.is_ok());
+///         assert(result.value() == 6);
+///         return AsyncOk(Void{});
+///     });
+///
+/// inline_executor.schedule_task(PendingTask(std::move(p)));
+/// ```
+class InlineExecutor final : public Executor, public boost::noncopyable {
+public:
+    InlineExecutor() noexcept : ctx_(this) {}
+
+    ~InlineExecutor() override = default;
+
+    void schedule_task(PendingTask task) override {
+        const bool finished = task(ctx_);
+        assert(finished == !task);
+        (void)finished;
+    }
+
+private:
+    class ContextImpl : public Context {
+    public:
+        explicit ContextImpl(InlineExecutor* executor) : executor_(executor) {}
+
+        ~ContextImpl() override = default;
+
+        InlineExecutor* get_executor() const override {
+            return executor_;
+        }
+
+        SuspendedTask suspend_task() override {
+            throw std::runtime_error("InlineExecutor doesn't support suspend");
+        }
+
+    private:
+        InlineExecutor* const executor_;
+    };
+
+    ContextImpl ctx_;
+};
+} // namespace bipolar
+
+#endif
