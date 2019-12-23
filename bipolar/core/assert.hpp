be2868d32ff259dc821778f3e0f074884c39af36
condy <condy0919@gmail.com>
2019-12-23 16:32:34 +0800

    [core] BIPOLAR_ASSERT is available

---
+++ bipolar/core/assert.hpp
@@ -0,0 +1,23 @@
+//!
+//!
+//!
+
+#ifndef BIPOLAR_CORE_ASSERT_HPP_
+#define BIPOLAR_CORE_ASSERT_HPP_
+
+#include "bipolar/core/logger.hpp"
+
+#ifndef NDEBUG
+#define BIPOLAR_ASSERT(expr, ...)                                              \
+    do {                                                                       \
+        if (!(expr)) {                                                         \
+            auto& logger =                                                     \
+                ::bipolar::Registry::get_logger(::bipolar::LoggerID::assert);  \
+            BIPOLAR_LOG_WARN(logger, __VA_ARGS__);                             \
+        }                                                                      \
+    } while (false)
+#else
+#define BIPOLAR_ASSERT(expr, msg) ((void)(expr))
+#endif
+
+#endif
